f0d08ad4e67c4913a5d411f9a6aa9ec632272e08
Peter Brotherwood <petebrotherwood@btinternet.com>
2022-07-19 10:23:10 -0400

    uploaded singularity image and updated slurm scripts

--- scripts/mriprep.sh
+++ scripts/mriprep.sh
@@ -3,7 +3,7 @@
 #SBATCH --time 24:0:0
 #SBATCH --account def-charesti
 #SBATCH --mem 32G
-#SBATCH --array 1-2
+#SBATCH --array 1-259
 
 set -e
 
@@ -12,6 +12,6 @@ module load singularity/3.8
 workingdir=$(echo "/home/pbro98/projects/def-charesti/pbro98/brotherwood_project")
 subject=$(sed -n ${SLURM_ARRAY_TASK_ID} ${workingdir}/valid_subjects.txt)
 
-singularity run --cleanenv fmriprep.simg ${workingdir}/data/ds00030 ${workingdir}/data/preprocessed participant --participant-label ${subject}
+singularity run --cleanenv ${workingdir}/images/fmriprep-21.0.2.simg ${workingdir}/data/ds00030 ${workingdir}/data/preprocessed participant --participant-label ${subject}
 
 
